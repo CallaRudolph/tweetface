6862aad338b8639a4115b7738bcafa27ec360a2d
Calla Rudolph and Nathan Stewart <nathan@>
2017-09-12 09:54:01 -0700

    add state to newtweetform comp, render alert when form input submitted

--- src/components/NewTweetForm.jsx
+++ src/components/NewTweetForm.jsx
@@ -1,25 +1,42 @@
 import React from "react";
 
-function NewTweetForm(props) {
-  return (
-    <div>
-      <form>
-        <input
-          type="text"
-          id="img"
-          placeholder="image URL"/>
-        <input
-          type="text"
-          id="title"
-          placeholder="enter title"/>
-        <textarea
-          type="text"
-          id="content"
-          placeholder="enter blessing"/>
-        <button type="submit">Send blessing.</button>
-      </form>
-    </div>
-  );
+class NewTweetForm extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.handleNewTweetSubmission = this.handleNewTweetSubmission.bind(this);
+  }
+
+  handleNewTweetSubmission(event) {
+    event.preventDefault();
+    const {_img, _title, _content} = this.refs;
+    alert(`Data! ${_img.value}, ${_title.value}, ${_content.value}`);
+  }
+
+  render(){
+    return (
+      <div>
+        <form onSubmit={this.handleNewTweetSubmission}>
+          <input
+            ref="_img"
+            type="text"
+            id="img"
+            placeholder="image URL"/>
+          <input
+            ref="_title"
+            type="text"
+            id="title"
+            placeholder="enter title"/>
+          <input
+            ref="_content"
+            type="text"
+            id="content"
+            placeholder="enter blessing"/>
+          <button type="submit">Send blessing.</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NewTweetForm;
